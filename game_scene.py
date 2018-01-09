e5348bfaff4e783d648cec6e1327aa71c995ac64
Malcolm <malcolm.mccarthy@ocsbstudent.ca>
2018-01-09 13:25:41 -0500

    Added buttons

--- game_scene.py
+++ game_scene.py
@@ -1,46 +1,84 @@
-# Created by: MALCOLM McCARTHY
-# Shows the game scene
+'''
+created by Malcolm McCarthy
+created in January 2018
+created for ICS3U
+this scene shows the main game
+'''
+
 from scene import *
 import ui
 
+from main_menu_scene import *
 
 class GameScene(Scene):
     def setup(self):
-        # this method is called, when user moves to this scene
         
-        # add background color
-        self.background = SpriteNode(position = self.size / 2, 
-                                     color = 'white', 
-                                     parent = self, 
-                                     size = self.size)
-    
+        self.center_of_screen = self.size/2
+        self.left_button_down = False
+        self.right_button_down = False
+        self.charcter_movement_speed = 22.5
+        
+        # adds the background texture
+        self.bg = Vector2()   
+        self.bg.x = self.size.x / 2
+        self.bg.y = self.size.y / 2
+        self.background = SpriteNode('./assets/sprites/football_field.JPG',
+                                     position = self.bg,
+                                     parent = self,
+                                     size = self.size,
+                                     scale = 1.15)
+                                     
+        character_position = self.center_of_screen
+        character_position.y = 100
+        self.character = SpriteNode('./assets/sprites/character.JPG',
+                                     parent = self,
+                                     position = character_position,
+                                     size = self.size / 5)
+        
+        move_left_button_position = self.center_of_screen
+        move_left_button_position.x = 100
+        self.move_left_button = SpriteNode('./assets/sprites/move_left_button.png',
+                                            parent = self,
+                                            position = move_left_button_position,
+                                            size = self.size / 6)
+                                     
+        move_right_button_position = self.center_of_screen
+        move_right_button_position.x = 900
+        self.move_right_button = SpriteNode('./assets/sprites/move_right_button.png',
+                                             parent = self,
+                                             position = move_right_button_position,
+                                             size = self.size / 6)
+        
+        pause_button_position = self.center_of_screen
+        pause_button_position.x = 100
+        pause_button_position.y = 675
+        self.pause = SpriteNode('./assets/sprites/pause_button.png',
+                                 parent = self,
+                                 position = pause_button_position,
+                                 size = self.size / 8) 
+        
     def update(self):
-        # this method is called, hopefully, 60 times a second
-        pass
-    
+        
+        # move spaceship if button is pressed down
+        if self.left_button_down == True:
+            self.character.run_action(Action.move_by(-1*self.charcter_movement_speed, 0.0, 0.1))
+        if self.right_button_down == True:
+            self.character.run_action(Action.move_by(self.charcter_movement_speed, 0.0, 0.1))
+            
     def touch_began(self, touch):
         # this method is called, when user touches the screen
-        pass
-    
-    def touch_moved(self, touch):
-        # this method is called, when user moves a finger around on the screen
-        pass
-    
+        
+        # check if left or right button is down
+        if self.move_left_button.frame.contains_point(touch.location):
+            self.left_button_down = True
+        
+        if self.move_right_button.frame.contains_point(touch.location):
+            self.right_button_down = True
+            
     def touch_ended(self, touch):
         # this method is called, when user releases a finger from the screen
-        pass
-    
-    def did_change_size(self):
-        # this method is called, when user changes the orientation of the screen
-        # thus changing the size of each dimension
-        pass
-    
-    def pause(self):
-        # this method is called, when user touches the home button
-        # save anything before app is put to background
-        pass
-    
-    def resume(self):
-        # this method is called, when user place app from background 
-        # back into use. Reload anything you might need.
-        pass
+        
+        # if I removed my finger, then no matter what spaceship
+        #    should not be moving any more
+        self.left_button_down = False
+        self.right_button_down = False
