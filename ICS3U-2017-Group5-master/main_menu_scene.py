5962c2b200c348c9b637e220d737880aa7f23667
Malcolm <malcolm.mccarthy@ocsbstudent.ca>
2018-01-19 13:22:20 -0500

    Changes

--- ICS3U-2017-Group5-master/main_menu_scene.py
+++ ICS3U-2017-Group5-master/main_menu_scene.py
@@ -8,6 +8,7 @@ import ui
 
 from game_scene import *
 from help_scene import *
+from about_scene import *
 
 class MainMenuScene(Scene):
     def setup(self):
@@ -26,23 +27,29 @@ class MainMenuScene(Scene):
                                      parent = self,
                                      size = self.size)                         
         start_button_position = self.size/2
-        start_button_position.y = start_button_position.y + 200
+        start_button_position.y = start_button_position.y + 100
         self.start_button = SpriteNode('./assets/sprites/start_button.png',
                                        parent = self,
                                        position = start_button_position)
        
         help_button_position = self.size/2
-        #help_button_position.y = help_button_position.y
+        help_button_position.y = help_button_position.y - 100
         self.help_button = SpriteNode('./assets/sprites/help_button.png',
                                        parent = self,
                                        position = help_button_position)
         
         about_button_position = self.size/2
-        about_button_position.y = about_button_position.y - 200
+        about_button_position.y = about_button_position.y - 300
         self.about_button = SpriteNode('./assets/sprites/about_button.png',
                                        parent = self,
                                        position = about_button_position)
-    
+        title_position = Vector2()
+        title_position.x = title_position.x + 500
+        title_position.y = title_position.y + 600
+        self.title= LabelNode(text = 'FOOTBALLER',
+                                  font = ('Copperplate', 80),
+                                  parent = self,
+                                  position = title_position )
     
     def update(self):
         # this method is called, hopefully, 60 times a second
@@ -60,24 +67,16 @@ class MainMenuScene(Scene):
         # this method is called, when user releases a finger from the screen
         # if the start button is pressed, the game scene will pop up
         if self.start_button.frame.contains_point(touch.location):
+            
             self.present_modal_scene(GameScene())
             
         # if help button is pressed, the help scene will pop up
         if self.help_button.frame.contains_point(touch.location):
+            
             self.present_modal_scene(HelpScene())
+        # if the about button is pressed, about scene will pop up
+        if self.about_button.frame.contains_point(touch.location):
+            
+            self.present_modal_scene(AboutScene())
     
-    def did_change_size(self):
-        # this method is called, when user changes the orientation of the screen
-        # thus changing the size of each dimension
-        pass
-    
-    def pause(self):
-        # this method is called, when user touches the home button
-        # save anything before app is put to background
-        pass
-    
-    def resume(self):
-        # this method is called, when user place app from background 
-        # back into use. Reload anything you might need.
-        pass
     
